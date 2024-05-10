123903dc4bfc5121b6a379b8fe7d49b63cd59ac4
johanperjulius1 <johan.per.julius.berglund@gmail.com>
2024-05-10 14:17:55 +0200

    finishing lecture 104

--- next-project/components/MainHeader/main-header.js
+++ next-project/components/MainHeader/main-header.js
@@ -1,26 +1,31 @@
 import Link from 'next/link';
+import Image from 'next/image';
 
-import logoImg from '@/assets/logo.png';
-import classes from './main-header.module.css';
+import MainHeaderBackground from '@/components/MainHeaderBackground/main-header-background';
+import logo from '@/assets/logo.png';
+import styles from './main-header.module.css';
 
 export default function MainHeader() {
   return (
-    <header className={classes.header}>
-      <Link className={classes.logo} href="/">
-        <img src={logoImg.src} alt="A plate with food on it" />
-        NextLevel Food
-      </Link>
+    <>
+      <MainHeaderBackground />
+      <header className={styles.header}>
+        <Link className={styles.logo} href="/">
+          <Image src={logo} priority alt="A plate with food on it" />
+          NextLevel Food
+        </Link>
 
-      <nav className={classes.nav}>
-        <ul>
-          <li>
-            <Link href="/meals">Browse Meals</Link>
-          </li>
-          <li>
-            <Link href="/community">Foodies Community</Link>
-          </li>
-        </ul>
-      </nav>
-    </header>
+        <nav className={styles.nav}>
+          <ul>
+            <li>
+              <Link href="/meals">Browse Meals</Link>
+            </li>
+            <li>
+              <Link href="/community">Foodies Community</Link>
+            </li>
+          </ul>
+        </nav>
+      </header>
+    </>
   );
 }
